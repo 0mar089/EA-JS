0c37b960d7763afef867e2b6b81dcdc414ff8a6f
0mar089 <omar.boury@estudiantat.upc.edu>
2026-02-17 19:25:35 +0100

    Ejercicio hecho

--- 5-exercise/exercise.js
+++ 5-exercise/exercise.js
@@ -9,11 +9,54 @@ fetch('https://jsonplaceholder.typicode.com/users/')
   .then(response => response.json())
   .then(users => {
       // YOUR CODE STARTS HERE
-      console.log("--- Processed Users ---");
+        console.log("--- Processed Users ---");
       // 1. Filter even IDs
+
+        const evenUsers = users.filter(user => user.id % 2 == 0)
+
       // 2. Map to clean objects {id, name, city}
+
+        const transformUsers = users.map(user => ({ // Esto significa que para cada usuario, me aplicas el codigo siguiente que viene despues de la flecha arrow. 
+                id: user.id,
+                name: user.name,                       // Al poner => ({}) indico que esto es un objeto y es lo que se devolverá y no es codigo
+                city: user.address.city
+        }));
+
       // 3. Add Guest User at the start using Spread (...)
+
+        const guest = {
+            id: 0,
+            name: 'Hannibal Lecter',
+            username: 'Hannibal',
+            email: 'hanniballecter@killer.com',
+            address: {
+                street: 'Hospital Street',
+                suite: 'Apt. 1',
+                city: 'Baltimore',
+                zipcode: '21201',
+                geo: { 
+                    lat: "-37", 
+                    lng: "84" 
+                }
+            },
+            phone: '1-770-746-8021 x56442',
+            website: 'hannibal.com',
+            company: {
+                name: 'FBI',
+                catchPhrase: 'Criminal profiling and analysis',
+                bs: 'catch high-profile serial killers'
+            }
+        };
+
+        const newUserInUsers = [guest,...users]
       
-      console.log("--- Statistics ---");
+        console.log("--- Statistics ---");
+
       // 4. Reduce to count total characters in names
+    
+      const resultado = newUserInUsers.reduce(function(total, user) {
+            return total += user.name.length;
+      }, 0);
+      
+        console.log(resultado);
   });
